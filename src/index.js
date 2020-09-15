1b2b60a37ac67ee6158968cb7b856c47bc76087b
themikaelson <34260590+themikaelson@users.noreply.github.com>
2020-09-15 18:17:35 -0400

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { Provider } from "react-redux";
+
+import { store, persistor } from "./redux/store";
+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById("root")
+);
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+>>>>>>> 2b322ba... Initialize project using Create React App
